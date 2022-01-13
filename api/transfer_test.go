3fb422ccee8f9f4d964a9b250bad3a801fbb7cc3
Anton Braer <xanzian@yandex.ru>
2022-01-13 16:37:41 +0300

    add transfers test

---
+++ api/transfer_test.go
@@ -0,0 +1,188 @@
+package api
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	mockdb "simplebank/db/mock"
+	db "simplebank/db/sqlc"
+	"simplebank/util"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateTransferAPI(t *testing.T) {
+	account1 := generateRandomAccount()
+	account2 := generateRandomAccount()
+	account1.Currency = util.USD
+	account2.Currency = util.USD
+	amount := int64(10)
+	transfer := generateRandomTransfer(account1.ID, account2.ID, amount)
+
+	testCases := []struct{
+		name string
+		body gin.H
+		buildStabs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"from_account_id": transfer.FromAccountID,
+				"to_account_id":   transfer.ToAccountID,
+				"amount":        transfer.Amount,
+				"currency":      account1.Currency,
+			},
+			buildStabs: func(store *mockdb.MockStore){
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account1.ID)).Times(1).Return(account1, nil)
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account2.ID)).Times(1).Return(account2, nil)
+
+				arg := db.TransferTxParams{
+					FromAccountID: transfer.FromAccountID,
+					ToAccountID:   transfer.ToAccountID,
+					Amount:        transfer.Amount,
+				}
+				store.EXPECT().TransferTx(gomock.Any(), gomock.Eq(arg)).Times(1)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder){
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+		{
+			name: "badBody",
+			body: gin.H{
+				"from_account_id": transfer.FromAccountID,
+				"to_account_id":   transfer.ToAccountID,
+				"amountt":        transfer.Amount, //double t
+				"currency":      account1.Currency,
+			},
+			buildStabs: func(store *mockdb.MockStore){
+				
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder){
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name: "InternalError",
+			body: gin.H{
+				"from_account_id": transfer.FromAccountID,
+				"to_account_id":   transfer.ToAccountID,
+				"amount":        transfer.Amount,
+				"currency":      account1.Currency,
+			},
+			buildStabs: func(store *mockdb.MockStore){
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account1.ID)).Times(1).Return(account1, nil)
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account2.ID)).Times(1).Return(account2, nil)
+
+				arg := db.TransferTxParams{
+					FromAccountID: transfer.FromAccountID,
+					ToAccountID:   transfer.ToAccountID,
+					Amount:        transfer.Amount,
+				}
+				store.EXPECT().TransferTx(gomock.Any(), gomock.Eq(arg)).Times(1).Return(db.TransferTxResult{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder){
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+		{
+			name: "acc1 not found",
+			body: gin.H{
+				"from_account_id": transfer.FromAccountID,
+				"to_account_id":   transfer.ToAccountID,
+				"amount":        transfer.Amount,
+				"currency":      account1.Currency,
+			},
+			buildStabs: func(store *mockdb.MockStore){
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account1.ID)).Times(1).Return(db.Account{}, sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder){
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			name: "acc2 not found",
+			body: gin.H{
+				"from_account_id": transfer.FromAccountID,
+				"to_account_id":   transfer.ToAccountID,
+				"amount":        transfer.Amount,
+				"currency":      account1.Currency,
+			},
+			buildStabs: func(store *mockdb.MockStore){
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account1.ID)).Times(1).Return(account1, nil)
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account2.ID)).Times(1).Return(db.Account{}, sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder){
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			name: "Get account InternalError",
+			body: gin.H{
+				"from_account_id": transfer.FromAccountID,
+				"to_account_id":   transfer.ToAccountID,
+				"amount":        transfer.Amount,
+				"currency":      account1.Currency,
+			},
+			buildStabs: func(store *mockdb.MockStore){
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account1.ID)).Times(1).Return(db.Account{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder){
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+		{
+			name: "Bad currency",
+			body: gin.H{
+				"from_account_id": transfer.FromAccountID,
+				"to_account_id":   transfer.ToAccountID,
+				"amount":        transfer.Amount,
+				"currency":      util.EUR,
+			},
+			buildStabs: func(store *mockdb.MockStore){
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account1.ID)).Times(1).Return(account1, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder){
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			store := mockdb.NewMockStore(ctrl)
+
+			tc.buildStabs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+			body, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+			url := "/transfers"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(body))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+
+			tc.checkResponse(t, recorder)
+		})
+	}
+}
+
+func generateRandomTransfer(account1ID, account2ID, amount int64) db.Transfer {
+	return db.Transfer{
+		ID:            util.RandomInt(1, 1000),
+		FromAccountID: account1ID,
+		ToAccountID:   account2ID,
+		Amount:        amount,
+	}
+}
--- go.mod
+++ go.mod
@@ -2,17 +2,21 @@ module simplebank
 
 go 1.17
 
-require github.com/lib/pq v1.10.4
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/golang/mock v1.6.0
+	github.com/lib/pq v1.10.4
+	github.com/spf13/viper v1.10.1
+	github.com/stretchr/testify v1.7.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
-	github.com/golang/mock v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -28,17 +32,11 @@ require (
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.10.1 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
-	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/sys v0.0.0-20220110181412-a018aaa089fe // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/tools v0.1.1 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
